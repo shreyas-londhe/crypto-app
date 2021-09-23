602d7f124ff62ee634445cd6c6c56205bf41195e
shryasss <shredya5101@gmail.com>
2021-09-23 18:58:46 +0530

    redux start

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/exchanges",
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "7aba9b0c8bmsha9443e76a2479b9p1cf017jsn80f8f3739f22",
+//   },
+// };
