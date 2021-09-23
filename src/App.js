5ad78441c23f86bf5d61807ab36eb5b69c616030
shryasss <shredya5101@gmail.com>
2021-09-23 17:23:54 +0530

    navbar chnages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+import "./App.css";
 
 import { Navbar } from "./components";
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,10 +17,23 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/"></Link>
+          <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-        {/* <Button className="menu-control-container"></Button> */}
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
